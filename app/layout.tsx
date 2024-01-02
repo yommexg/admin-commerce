2bc7accf2efbf4a8b903e0d85952f0898b48eed6
Bolu Yomi-olugbodi  (Anime Code Lord) <98047628+yommexg@users.noreply.github.com>
2024-01-02 10:34:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             <ToasterProvider />
             <ModalProvider />
           </ThemeProvider>
